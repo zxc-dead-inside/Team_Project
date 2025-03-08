c5c51a575f00c886d01bb2d5dba6f7c54d7ef84d
Журавлев Константин Олегович <huravlev@alfabank.ru>
2025-03-09 02:35:38 +0300

    Fixed router import

--- app/auth-service/src/api/auth.py
+++ app/auth-service/src/api/auth.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from src.api.v1.login import protected_router
+from src.api.v1.login import private_router
 from src.api.v1.login import public_router
 
 auth_router = APIRouter()
 
 auth_router.include_router(
-    protected_router, prefix="/auth", tags=["Authentication"])
+    private_router, prefix="/auth", tags=["Authentication"])
 auth_router.include_router(
     public_router, prefix="/auth", tags=["Authentication"])

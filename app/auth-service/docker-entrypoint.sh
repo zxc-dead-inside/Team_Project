c6dcf3b4a6c54af0095bf6dbcdd78cd0564921c8
E.D. <54731451+Edudeiko@users.noreply.github.com>
2025-03-22 13:06:47 -0400

    issue-107: Code review updates part 1 and 2 (#109)
    * issue-107: Code review updates part 1 and 2
    
    * issue-107: Code review updates part 1 and 2 v2
    
    * issue-107: Code review updates part 1 and 2 v3

--- app/auth-service/docker-entrypoint.sh
+++ app/auth-service/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
-# Generate keys if they don't exist
+echo "Checking and creating RS256 keys if needed..."
 python scripts/create_rs256_keys.py
+
+# Set up database and run necessary scripts
+echo "Setting up database..."
+python scripts/setup_database.py
 
 # Execute the main command
 exec "$@"

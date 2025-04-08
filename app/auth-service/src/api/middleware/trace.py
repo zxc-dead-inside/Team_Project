0effc69087645f578fb6944b31599af9b6d9f1f2
zxc-dead-inside <maks.arhipkin@mail.ru>
2025-04-08 23:11:57 +1000

    auth tracer

--- app/auth-service/src/api/middleware/trace.py
+++ app/auth-service/src/api/middleware/trace.py
@@ -1,35 +1,18 @@
-import random
-
-from fastapi import Request
 from opentelemetry import trace
+from opentelemetry.propagate import extract
 from opentelemetry.context import attach, detach
-from opentelemetry.trace import set_span_in_context, SpanContext, TraceFlags
 from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request
 
 
 class TraceParentMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        token = None
+        context = extract(request.headers)
+        token = attach(context)
 
-        if "traceparent" not in request.headers:
-            new_trace_id = random.getrandbits(128)
-            new_span_id = random.getrandbits(64)
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span("incoming-request"):
+            response = await call_next(request)
 
-            span_context = SpanContext(
-                trace_id=new_trace_id,
-                span_id=new_span_id,
-                is_remote=False,
-                trace_flags=TraceFlags(TraceFlags.SAMPLED),
-                trace_state={}
-            )
-            span = trace.NonRecordingSpan(span_context)
-
-            context = set_span_in_context(span)
-            token = attach(context)
-
-        response = await call_next(request)
-
-        if token:
-            detach(token)
-
+        detach(token)
         return response
--- app/auth-service/src/tracing.py
+++ app/auth-service/src/tracing.py
@@ -2,29 +2,28 @@ from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace.sampling import ALWAYS_ON
 
 
 def setup_tracer(app):
-    trace.set_tracer_provider(
-        TracerProvider(
-            resource=Resource.create({SERVICE_NAME: "auth-service"})
-        )
+    provider = TracerProvider(
+        sampler=ALWAYS_ON,  # default sampler
+        resource=Resource.create({SERVICE_NAME: "auth-service"})
+    )
+    trace.set_tracer_provider(provider)
+
+    otlp_exporter = OTLPSpanExporter(
+        endpoint="jaeger:4317",
+        insecure=True,
     )
 
-    jaeger_exporter = JaegerExporter(
-        agent_host_name="jaeger",
-        agent_port=6831,
-    )
+    span_processor = BatchSpanProcessor(otlp_exporter)
+    provider.add_span_processor(span_processor)
 
-    span_processor = BatchSpanProcessor(jaeger_exporter)
-    trace.get_tracer_provider().add_span_processor(span_processor)
-
-    FastAPIInstrumentor.instrument_app(app)
-
-    RequestsInstrumentor().instrument()
-
+    FastAPIInstrumentor().instrument_app(app, tracer_provider=provider)
+    RequestsInstrumentor().instrument(tracer_provider=provider)
     LoggingInstrumentor().instrument(set_logging_format=True)

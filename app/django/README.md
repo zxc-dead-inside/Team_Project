b5c174c14884fd8401889c6d818c86fa3dac45ea
Edudeiko <evgeniy.dudeyko@gmail.com>
2025-06-18 03:38:15 +0000

    sprint-9: Issue-137 Added ELK setup

---
+++ app/django/README.md
@@ -0,0 +1,11 @@
+# Access django app in deployment
+
+1. To login to the admin
+
+http://localhost/admin/
+
+2. Check all the movies
+
+http://localhost/api/v1/movies/
+
+
--- app/fastapi/main.py
+++ app/fastapi/main.py
@@ -1,20 +1,40 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.api import api_router
 from core.config import settings
-from db.redis import redis_connector
 from db.elastic import es_connector
+from db.redis import redis_connector
+
+from core.logging_setup import StructuredLogger, setup_logging
+from middleware.logging import logging_middleware
+
+
+setup_logging()
+app_logger = StructuredLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(api: FastAPI):
-    await redis_connector.connect()
-    await es_connector.connect()
-    yield
-    await redis_connector.disconnect()
-    await es_connector.disconnect()
+    app_logger.info("Starting FastAPI application", service="fastapi")
+    try:
+        await redis_connector.connect()
+        app_logger.info("Redis connected successfully")
+
+        await es_connector.connect()
+        app_logger.info("Elasticsearch connected successfully")
+
+        yield
+
+    except Exception as e:
+        app_logger.error("Error during startup", error=e)
+        raise
+    finally:
+        app_logger.info("Shutting down FastAPI application")
+        await redis_connector.disconnect()
+        await es_connector.disconnect()
+
 
 app = FastAPI(
     title=settings.project_name,
@@ -23,8 +43,11 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc",
     version="1.0.0",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
+
+# Logging middleware
+app.middleware("http")(logging_middleware)
 
 # Set CORS middleware
 app.add_middleware(
